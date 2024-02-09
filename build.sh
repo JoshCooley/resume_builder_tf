572f262fcc71866ac91732c0d6dff6c46c0b9b63
Josh Cooley <josh@cooley.tech>
2024-02-09 14:58:38 -0600

    Reformat build.sh and add PDF generation step

--- build.sh
+++ build.sh
@@ -3,6 +3,18 @@ set -o errexit
 
 terraform init -input=false -lock=false
 terraform apply -auto-approve -lock=false
-
-yarn install --frozen-lockfile --no-progress --non-interactive
-yarn run showdown makehtml --input resume.md --output resume.html
+echo
+yarn install \
+  --frozen-lockfile \
+  --no-progress \
+  --non-interactive
+yarn run showdown makehtml \
+  --config disableForced4SpacesIndentedSublists \
+  --input resume.md \
+  --output resume.html
+echo
+chromium \
+  --headless \
+  --disable-gpu \
+  --no-pdf-header-footer \
+  --print-to-pdf=resume.pdf resume.html
